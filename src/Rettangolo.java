3499017f50818cc278f9c1640c98d257324a01ba
AlbanoGiuseppePersechino <albano.persechino@gmail.com>
2024-04-10 14:47:50 +0200

    correzione esercizio

--- src/Rettangolo.java
+++ src/Rettangolo.java
@@ -1,12 +1,26 @@
 class Rettangolo extends Forma {
-    private final double base;
-    private final double altezza;
+    private double base;
+    private double altezza;
 
     public Rettangolo(double base, double altezza) {
         this.base = base;
         this.altezza = altezza;
     }
+    public double getBase() {
+        return base;
+    }
 
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
     @Override
     public double calcolaArea() {
         return base * altezza;
